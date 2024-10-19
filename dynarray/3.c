58e88654bb33b16aa65a4ccf51a499eeca144e80
Arqaaa <166951155+3nruu@users.noreply.github.com>
2024-10-19 15:05:33 +0300

    hw

---
+++ dynarray/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+#ifdef COUNT
+
+    for (int i = 1; i <= COUNT; i++) 
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+#else
+    printf("No Count!\n");
+#endif
+    return 0;
+}
